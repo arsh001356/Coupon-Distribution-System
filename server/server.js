6338125e8d263c42cf78a199345b85e676feae67
arsh001356 <arsh001356@gmail.com>
2025-03-16 18:51:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5174',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    origin: "*", 
+    credentials: true, 
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 // Session ID middleware
